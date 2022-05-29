faeea7903cbb120356806e54e6402e8a72a0a05c
swh <sungwoo166@gmail.com>
2022-05-29 18:20:56 +0900

    setState
    .

--- reprc/src/App.js
+++ reprc/src/App.js
@@ -1,25 +1,46 @@
+import {Component} from "react"; //클래스
+
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    //로컬 state 이 컴포넌트만 사용가능
+    constructor() { //when ever its built // 다른 생성자들이랑 똑같다
+        super();
+        this.state = {
+            name: {lastName: "SW", firstName: "Hong"},
+
+            company: "Baek Sue"
+        };
+
+    }
+    // name: {firstName: "Sam", lastName: "Park"
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Hi I'm {this.state.name.firstName} {this.state.name.lastName}, I am {this.state.company}
+                    </p>
+
+                    <button
+                        onClick={()=>{
+                            this.setState((state, props)=>{
+                                return {
+                                    name: {firstName: "Sam", lastName: "Park"}
+                                }
+                            }, ()=>{ console.log(this.state)});
+                        }}
+                    >
+                        Change Name
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- reprc/src/index.js
+++ reprc/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; // 애플리케이션 빌더, 엔진
+import ReactDOM from 'react-dom/client'; // dom 만들것이다.
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//public/index.html 에서 root id 가진곳 root에 저장
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+root.render( //root 에 랜더링을 해라
   <React.StrictMode>
-    <App />
+    <App />  // 전체 리액트앱
   </React.StrictMode>
 );
 
