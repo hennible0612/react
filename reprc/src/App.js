7e872c3f208a173f3735f301aef1e4840d8830a5
swh <sungwoo166@gmail.com>
2022-06-09 15:40:31 +0900

    useEffect
    .

--- reprc/src/App.js
+++ reprc/src/App.js
@@ -1,69 +1,114 @@
-import {Component} from "react"; //클래스
-
-
-import logo from './logo.svg';
 import './App.css';
+
+import {useState, useEffect} from 'react'
 
 import './components/card-list/card-list.component'
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 // import CardListComponent from "./components/card-list/card-list.component";
 
-class App extends Component {
+const App = () => {
+    const [searchField, setSearchField] = useState(''); // [value, setValue]
+    const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilterMonsters] = useState(monsters)
 
-    constructor() {
-        console.log("Constructor")
 
-        super();
-        this.state = {
-            monsters: [],
-            searchField: ''
-        };
+    //dependency 에 있는 값이 바뀔때만 rendering 한다. 처음에는 걍 돌린다.
+    useEffect(() => {
+        console.log('useEffect');
 
-    }
-
-    componentDidMount() {
-        console.log("ComponenetDidMount")
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
-            .then((users) => this.setState(
-                    () => {
-                        return {monsters: users}
-                    },
-                    () => {
-                        // console.log(this.state)
-                    }
-                )
-            );
+            .then((users) => setMonsters(users));
+
+    }, [])
+
+    console.log('render');
+    useEffect(() => {
+        const newfilteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchField)
+        });
+
+        setFilterMonsters(newfilteredMonsters);
+    },[monsters, searchField]);
+
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase(); //입력된값
+        setSearchField(searchFieldString);
+
     }
 
-    // 검색 필터 매서드
-    onSearchChange = (event) => {
 
-        const searchField = event.target.value.toLocaleLowerCase();
-
-        this.setState(() => {
-            return {searchField: searchField}
-        });
-    }
-
-    render() {
-        console.log("render")
-        const {monsters, searchField} = this.state;
-        const {onSearchChange} = this
-
-        const filteredMonsters = monsters.filter((monster) => { //검색된 array
-            return monster.name.toLocaleLowerCase().includes(searchField); //있을시 true 그리고 array에 저장장                    })
-        });
-        console.log(filteredMonsters)
-        return (<div className='App'>
+    return (
+        <div className='App'>
             <h1 className="app-title">Monsters Rolodex</h1>
 
-            <SearchBox className={"monsters-search-box"} onChangeHandler={onSearchChange} placeholder={'search Monsters'}/>
+            <SearchBox className={"monsters-search-box"}
+                       onChangeHandler={onSearchChange}
+                       placeholder={'search Monsters'}
+            />
 
             <CardList monsters={filteredMonsters}/>
-        </div>);
-    }
+        </div>
+    )
 }
+
+
+// class App extends Component {
+//
+//     constructor() {
+//         console.log("Constructor")
+//
+//         super();
+//         this.state = {
+//             monsters: [],
+//             searchField: ''
+//         };
+//
+//     }
+//
+//     componentDidMount() {
+//         console.log("ComponenetDidMount")
+//         fetch('https://jsonplaceholder.typicode.com/users')
+//             .then((response) => response.json())
+//             .then((users) => this.setState(
+//                     () => {
+//                         return {monsters: users}
+//                     },
+//                     () => {
+//                         // console.log(this.state)
+//                     }
+//                 )
+//             );
+//     }
+//
+//     // 검색 필터 매서드
+//     onSearchChange = (event) => {
+//
+//         const searchField = event.target.value.toLocaleLowerCase();
+//
+//         this.setState(() => {
+//             return {searchField: searchField}
+//         });
+//     }
+//
+//     render() {
+//         console.log("render")
+//         const {monsters, searchField} = this.state;
+//         const {onSearchChange} = this
+//
+//         const filteredMonsters = monsters.filter((monster) => { //검색된 array
+//             return monster.name.toLocaleLowerCase().includes(searchField); //있을시 true 그리고 array에 저장장                    })
+//         });
+//         console.log(filteredMonsters)
+//         return (<div className='App'>
+//             <h1 className="app-title">Monsters Rolodex</h1>
+//
+//             <SearchBox className={"monsters-search-box"} onChangeHandler={onSearchChange} placeholder={'search Monsters'}/>
+//
+//             <CardList monsters={filteredMonsters}/>
+//         </div>);
+//     }
+// }
 
 export default App;
