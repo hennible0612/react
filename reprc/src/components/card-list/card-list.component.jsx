dbf69ef27c38112ee5a32b0313c5d308c7c6359d
swh <sungwoo166@gmail.com>
2022-06-09 21:42:51 +0900

    components function 화
    .

--- reprc/src/components/card-list/card-list.component.jsx
+++ reprc/src/components/card-list/card-list.component.jsx
@@ -2,22 +2,19 @@ import {Component} from "react";
 import './card-list.styles.css'
 import CardContainer from "../card-container/card-container.component"
 
-class CardList extends Component {
-    render() {
-        const {monsters} = this.props
-        return (
-            <div className='card-list'>
-                {monsters.map(monster => {
-                    const {name, id, email} = monster
-                        return (
-                            <CardContainer monster={monster}/>
-                            // <CardContainer name={name} id={id} email={email}/>
+const CardList = ({monsters}) => (
+    // const {monsters} = props;
 
-                        )
-                    })}
-            </div>
-        );
-    }
-}
+    <div className='card-list'>
+        {monsters.map(monster => {
+            const {name, id, email} = monster
+            return (
+                <CardContainer monster={monster}/>
+                // <CardContainer name={name} id={id} email={email}/>
+
+            )
+        })}
+    </div>
+)
 
 export default CardList; // 다른 파일들이 import 할 수 있게 한다.
--- reprc/src/index.js
+++ reprc/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react'; // 애플리케이션 빌더, 엔진
-import ReactDOM from 'react-dom/client'; // dom 만들것이다.
+import { createRoot } from 'react-dom/client'; // dom 만들것이다.
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//public/index.html 에서 root id 가진곳 root에 저장
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render( //root 에 랜더링을 해라
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
+
+//public/index.html 에서 root id 가진곳 root에 저장
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+//
+// root.render( //root 에 랜더링을 해라
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
